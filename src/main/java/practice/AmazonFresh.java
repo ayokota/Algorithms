a64852a4efec73ffacca442574abe8eb2b1ba9eb
ayokota <akiyo.yokota@gmail.com>
2020-08-08 10:00:08 -0700

    new

--- src/main/java/practice/AmazonFresh.java
+++ src/main/java/practice/AmazonFresh.java
@@ -1,8 +1,5 @@
 package practice;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by ayokota on 8/4/20.
  */
@@ -34,45 +31,6 @@ public class AmazonFresh {
         }
         return 0;
     }
-
-    public static int azfresh(String[][] combos, String[] shoppingCart) {
-        int nextIndex = 0;
-        for(String[] combo : combos) {
-            int idx = azfreshHelper(combo, shoppingCart, 0, nextIndex, new HashSet<>());
-            if(idx == -1)
-                return 0;
-            nextIndex = idx;
-            for(int i = 0; i < combo.length; i++) {
-                shoppingCart[idx--] = "";
-            }
-        }
-
-        return 1;
-    }
-
-    public static int azfreshHelper(String[] combo, String[] shoppingCart, int comboIndex, int shoppingCartIndex, Set<String> visitedKeys) {
-        String key = comboIndex + ":" + shoppingCartIndex;
-        if(visitedKeys.contains(key))
-            return -1;
-
-
-        visitedKeys.add(key);
-
-        if(shoppingCartIndex >= shoppingCart.length || comboIndex >= combo.length) {
-            return -1;
-        }
-
-        if((combo[comboIndex] == shoppingCart[shoppingCartIndex] || combo[comboIndex].equals("anything"))
-                && comboIndex == combo.length - 1) {
-            return shoppingCartIndex;
-        } else if (combo[comboIndex] == shoppingCart[shoppingCartIndex] || combo[comboIndex].equals("anything")) {
-            return Math.max(azfreshHelper(combo, shoppingCart, comboIndex + 1, shoppingCartIndex + 1, visitedKeys),
-                    azfreshHelper(combo, shoppingCart, 0, shoppingCartIndex + 1, visitedKeys));
-        }
-
-        return azfreshHelper(combo, shoppingCart, 0, shoppingCartIndex + 1, visitedKeys);
-    }
-
 
     public static void main(String[] args) {
         String[][] combos1 = new String[][] {
